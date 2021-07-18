aca31a32943b9375d3bdae4d4efafa345efdf2b9
sakshamarora1 <sakshamarora1001@gmail.com>
2021-07-18 23:36:32 +0530

    Add github action

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from background import handwrite_background
 
 
 def on_starting(server):
-    t = threading.Thread(target=handwrite_background)
+    t = threading.Thread(target=handwrite_background, args=(True,))
     t.start()
